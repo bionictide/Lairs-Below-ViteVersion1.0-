0a950763fd923939e69179cf9c524b2c7701213f
BionicTide <robertwilldew@gmail.com>
2025-05-11 04:22:12 -0500

    REFACTOR ATTEMPT 4 TEST 7- SHARED EVENTS

--- StuffForAI/server_skeleton.js
+++ StuffForAI/server_skeleton.js
@@ -5,7 +5,7 @@
 import { Server } from 'socket.io';
 import http from 'http';
 import dotenv from 'dotenv';
-import { EVENTS } from './events.js';
+import { EVENTS } from '../src/shared/events.js';
 
 dotenv.config();
 
